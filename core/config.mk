d3d4e49fafdcd4e1ebd9a28567a76a1eaaa76cfe
DroidFreak32 <rushabshah32@gmail.com>
2017-10-15 17:07:21 +0000

    Cafing test

--- core/config.mk
+++ core/config.mk
@@ -887,6 +887,7 @@ include $(BUILD_SYSTEM)/soong_config.mk
 endif
 
 # Rules for QCOM targets
--include $(BUILD_SYSTEM)/qcom_utils.mk
+include $(BUILD_SYSTEM)/qcom_utils.mk
+include $(TOPDIR)build/make/core/qcom_target.mk
 
 include $(BUILD_SYSTEM)/dumpvar.mk
